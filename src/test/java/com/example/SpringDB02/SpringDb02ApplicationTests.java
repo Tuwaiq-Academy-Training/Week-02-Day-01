8c51414c365242a934dcf751d10e4a212512dacf
Lina Aloqaily <ogililina@gmail.com>
2022-05-31 05:02:53 +0300

    homeWork

---
+++ src/test/java/com/example/SpringDB02/SpringDb02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDB02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDb02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
